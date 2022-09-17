a42139dea29479bc93246d2133201062ce1742ff
programmeurs05 <111407738+programmeurs05@users.noreply.github.com>
2022-09-17 00:19:24 -0500

    Update TransactionService.java

--- Empresa/src/main/java/com/Programmeurs/Empresa/Controllers/TransactionService.java
+++ Empresa/src/main/java/com/Programmeurs/Empresa/Controllers/TransactionService.java
@@ -1,4 +1,53 @@
 package com.Programmeurs.Empresa.Controllers;
 
-public class TransactionService {
+import com.Programmeurs.Empresa.Entities.Employee;
+import com.Programmeurs.Empresa.Entities.Enterprise;
+import com.Programmeurs.Empresa.Entities.Transaction;
+import com.Programmeurs.Empresa.Services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.math.BigDecimal;
+
+
+@RestController
+@RequestMapping (path = "/enterprises/{transactionId}/movements")
+public class TransactionController {
+
+    TransactionService transactionService;
+
+    @Autowired
+    public TransactionController (TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public Transaction getTransaction(@PathVariable("transactionId") Long transactionId) {
+        return transactionService.getTransaction(transactionId);
+    }
+
+    @PostMapping
+    public void RegisterNewTransaction (@RequestBody Transaction transaction){
+        transactionService.addNewTransaction(transaction);
+    }
+
+
+    @PatchMapping
+    public void updateTransaction(@PathVariable("transactionId") Long transactionId,
+                                  @RequestParam(required = false) String concept,
+                                  @RequestParam(required = false) BigDecimal amount,
+                                  @RequestParam(required = false) Enterprise enterprise,
+                                  @RequestParam(required = false) Employee employee
+
+    ){
+        transactionService.updateTransaction(transactionId, concept, amount, enterprise, employee);
+    }
+
+    @DeleteMapping
+    public void deleteTransaction(@PathVariable("transactionId") Long transactionId){
+        transactionService.deleteTransaction(transactionId);
+    }
+
 }
+
